a2e32915c2829f83731e47c4bd2c865bd8e7e8db
Edvinas1982 <79442331+Edvinas1982@users.noreply.github.com>
2022-12-16 16:10:49 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// Turime masyvą, kurio kiekvienas elementas yra masyvas iš keturių elementų:
+// a) data kaip tekstinė eilutė, pvz: ‘2017-07-06 19:59:45’
+// b) automobilio numeris, pvz.: ‘LRS123’
+// c) automobilio nuvažiuotas kelias metrais
+// d) automobilio sugaištas laikas sekundėmis
+// Išveskite visus automobilius į html lentelę kartu su suskaičiuotu automobilio greičiu
+// kilometrais per valandą.
+
+var masyvas = [
+    ['2017-07-06 19:59:45', 'LRS123', 1500, 28],
+    ['2018-12-14 1:05:00', 'KOK331', 2520, 71],
+    ['1997-05-28 14:22:09', 'AZS444', 9500, 381],
+    ['1922-04-18 7:45:12', 'WOW999', 300, 89]
+]
+
+// Attempt nr3
+
---
+++ readme.md
@@ -0,0 +1,9 @@
+UŽDUOTIS
+Turime masyvą, kurio kiekvienas elementas yra masyvas iš keturių elementų:
+a) data kaip tekstinė eilutė, pvz: ‘2017-07-06 19:59:45’
+b) automobilio numeris, pvz.: ‘LRS123’
+c) automobilio nuvažiuotas kelias metrais
+d) automobilio sugaištas laikas sekundėmis
+Išveskite visus automobilius į html lentelę kartu su suskaičiuotu automobilio greičiu
+kilometrais per valandą.
+
